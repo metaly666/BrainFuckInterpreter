0ed4feac7b5744a1da9296459f520179bd43fc74
Dmitriy Kondak <dmitriykondak@gmail.com>
2020-11-21 19:49:18 +0200

    added testEmptyTaskList method in ParserTest

--- src/test/java/com/kondak/implementation/ParserTest.java
+++ src/test/java/com/kondak/implementation/ParserTest.java
@@ -3,12 +3,15 @@ package com.kondak.implementation;
 import com.kondak.commands.Command;
 import com.kondak.commands.Component;
 import com.kondak.commands.IncrementCommand;
+import com.kondak.commands.nodes.LeftBracketCommand;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -60,5 +63,20 @@ class ParserTest {
         String CODE = ">";
 
         assertArrayEquals(commandSet.toArray(), parser.getTaskList(CODE, commandSet).toArray());
+    }
+
+    @DisplayName("throw ArrayIndexOutOfBoundsException when tasks count < 0.")
+    @Test
+    void testEmpyTaskList() {
+        Parser parser = new Parser();
+        String CODE = "[[";
+
+        Command command = new LeftBracketCommand();
+        Set<Command> commandSet = new HashSet<>();
+        commandSet.add(command);
+
+        assertThrows(ArrayIndexOutOfBoundsException.class,
+                () -> parser.getTaskList(CODE, commandSet),
+                "Task list is null");
     }
 }
