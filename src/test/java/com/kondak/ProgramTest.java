01e4b8ff40fcb1e5cf82a4680cfed5c7e2dff4b1
Dmitriy Kondak <dmitriykondak@gmail.com>
2020-11-21 16:11:29 +0200

    added ProgramTest

---
+++ src/test/java/com/kondak/ProgramTest.java
@@ -0,0 +1,46 @@
+package com.kondak;
+
+import com.kondak.environment.Environment;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ProgramTest {
+    private static final Engine engine = new Engine();
+    private String CODE;
+
+    @Test()
+    void runHelloWorldCode() {
+        String expected = "Hello World!\n";
+
+        CODE = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+        engine.start(CODE);
+
+        Environment environment = Environment.getInstance();
+        String actual = environment.getOutputArr();
+
+        assertArrayEquals(expected.toCharArray(), actual.toCharArray());
+    }
+
+    @Test()
+    void runNestedBrackets() {
+        Environment environment = Environment.getInstance();
+        environment.reset();
+        CODE = "+[>[>]<-]";
+        engine.start(CODE);
+    }
+
+    @DisplayName("throw ArrayIndexOutOfBoundsException when cursor > 30_000.")
+    @Test
+    void testRightShiftOutOfBounds() {
+        Environment environment = Environment.getInstance();
+        environment.reset();
+        CODE = "+[>]";
+
+        assertThrows(ArrayIndexOutOfBoundsException.class,
+                () -> engine.start(CODE),
+                "Max index is " + (30_000 - 1));
+    }
+}
